2107fbc2230610f35799d98ed26b934efe91d0ba
Romidorka <artemvasnev01@gmail.com>
2023-02-25 20:55:13 +0600

    Removed duplicated import

--- app/src/main/java/com/example/medicineremindernew/LoginActivity.kt
+++ app/src/main/java/com/example/medicineremindernew/LoginActivity.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import com.example.medicineremindernew.firebase.UserData
 import com.example.medicineremindernew.firebase.UsersManager
 import java.security.MessageDigest
