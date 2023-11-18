1b51a89ac673b4585bda81baa5fd0c329f73aa0b
Romidorka <46569054+Romidorka@users.noreply.github.com>
2023-11-18 22:36:40 +0600

    Delete MedicineReminder-master directory

--- MedicineReminder-master/app/src/main/java/com/example/medicineremindernew/firebase/UserData.kt
+++
@@ -1,7 +0,0 @@
-package com.example.medicineremindernew.firebase
-
-data class UserData(
-        var userId: String? = null,
-        var userLogin: String? = null,
-        var userPassword: String? = null
-)
